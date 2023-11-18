9dc17b3ade91d1ab2304a996b4a923933f27de03
amber <anaya7864@gmail.com>
2023-11-18 13:47:45 +0500

    added usestate hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
 // import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 // import { LocalizationProvider } from '@mui/x-date-pickers';
@@ -6,23 +7,44 @@ import { PieChart } from '@mui/x-charts/PieChart';
 // import { DateRangePicker } from '@mui/x-date-pickers/DateRangePicker';
 
 export default function Home() {
+    const [fundsValue, setFundsValue] = useState(60);
+    const [expensesValue, setExpensesValue] = useState(40);
+
     const chartData = [
         {
             data: [
-                { id: 0, value: 60, label: 'Funds' },
-                { id: 1, value: 40, label: 'Expenses' },
+                { id: 0, value: fundsValue, label: 'Funds' },
+                { id: 1, value: expensesValue, label: 'Expenses' },
             ],
         },
-    ]
+    ];
+
     return (
         <div className='homepage-container'>
             <h1>Stats of Records</h1>
+            <div>
+                <label>Funds:</label>
+                <input
+                    type="number"
+                    value={fundsValue}
+                    onChange={(e) => setFundsValue(e.target.value)}
+                />
+            </div>
+            <div>
+                <label>Expenses:</label>
+                <input
+                    type="number"
+                    value={expensesValue}
+                    onChange={(e) => setExpensesValue(e.target.value)}
+                />
+            </div>
             {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DemoContainer components={['DateRangePicker']}>
                     <DateRangePicker localeText={{ start: 'Start Date', end: 'End Date' }} />
                 </DemoContainer>
             </LocalizationProvider> */}
             <PieChart
+            
                 className="chart"
                 series={chartData}
                 width={600}
